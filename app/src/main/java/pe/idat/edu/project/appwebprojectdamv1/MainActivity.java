74b24232bfd1f910686e82d973d816fb4bef4d4b
MartinMNapan <a20204009@idat.edu.pe>
2022-04-10 12:30:35 -0500

    Initial commit

---
+++ app/src/main/java/pe/idat/edu/project/appwebprojectdamv1/MainActivity.java
@@ -0,0 +1,18 @@
+package pe.idat.edu.project.appwebprojectdamv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Ingresar los parametros
+
+    }
+
+}
