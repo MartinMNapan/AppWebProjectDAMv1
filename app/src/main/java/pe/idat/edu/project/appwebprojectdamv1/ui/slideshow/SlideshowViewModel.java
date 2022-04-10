1110cf2790eed23b14a1f00f162b194df9a28265
MartinMNapan <a20204009@idat.edu.pe>
2022-04-10 17:08:58 -0500

    se creo segundo activite para el menu principal

---
+++ app/src/main/java/pe/idat/edu/project/appwebprojectdamv1/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package pe.idat.edu.project.appwebprojectdamv1.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
