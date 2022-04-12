4409897923d4f86ddf2fb678bad104f3398c6df0
MartinMNapan <a20204009@idat.edu.pe>
2022-04-12 18:51:06 -0500

    Agregando BDRide, y panel de registro de usuario

--- app/src/main/java/pe/idat/edu/project/appwebprojectdamv1/MainActivity.java
+++ app/src/main/java/pe/idat/edu/project/appwebprojectdamv1/MainActivity.java
@@ -3,8 +3,15 @@ package pe.idat.edu.project.appwebprojectdamv1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //
+    EditText user, pass, repass;
+    Button btning, btnreg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Ingresar los parametros
+        user = (EditText)findViewById(R.id.txtUsuario);
+        pass = (EditText)findViewById(R.id.txtContraseña);
+        repass = (EditText)findViewById(R.id.txtRContraseña);
+        btning = (Button)findViewById(R.id.btnIngresar);
+        btnreg = (Button)findViewById(R.id.btnRegistrar);
+
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
     }
 
